6cb168b87f1b059b6d58753579c5a326db44feb9
RahulMahto597 <112896323+RahulMahto597@users.noreply.github.com>
2022-09-05 23:18:14 +0530

    Create Program2.java

---
+++ Program2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;  
+public class Series  
+{  
+public static void main(String[] args)   
+{  
+int a, i;  
+Scanner sc=new Scanner(System.in);  
+System.out.print("Enter the limit: ");  
+a = sc.nextInt();    
+i=1;   
+System.out.print("Series of numbers: ");  
+while(i<=a)  
+{  
+//prints the odd number  
+System.out.print(i +" ");   
+i=i+2;  
+}     
+}  
+}  
